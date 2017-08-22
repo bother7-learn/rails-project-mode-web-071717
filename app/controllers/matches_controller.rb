68332f3a2ca15e24c3b9a9af7b885cabd134e874
SchruteFarms23 <romanmullady@gmail.com>
2017-08-22 16:37:39 -0400

    match create complete

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -7,6 +7,25 @@ class MatchesController < ApplicationController
   end
 
   def create
+
     @match = Match.new
+    if params[:home_user_team_id] == ""
+      home = Team.find(params[:home_team_id])
+    else
+      home = UserTeam.find(params[:home_user_team_id])
+    end
+
+    if params[:away_user_team_id] == ""
+      away = Team.find(params[:away_team_id])
+    else
+      away = UserTeam.find(params[:away_user_team_id])
+    end
+   # home = Team.find(params[:home_team_id]) or UserTeam.find(params[:home_user_team_id])
+   # away = Team.find(params[:away_team_id]) or UserTeam.find(params[:away_user_team_id])
+   @match.hometeam = home
+   @match.awayteam = away
+   
+   @match.simulate
+   @match.save
   end
 end
