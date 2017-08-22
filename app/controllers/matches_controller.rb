08c631af79bcd0ef5b85d67c59767420218dc671
bother7 <github email address>
2017-08-22 16:43:21 -0400

    trickey

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -9,12 +9,23 @@ class MatchesController < ApplicationController
   def create
 
     @match = Match.new
-    # params[:hometeam_id] params[:awayteam_id]
-    # home = Team.find(params[:hometeam_id]) or UserTeam.find(params[:hometeam_id])
-    # away = Team.find(params[:awayteam_id]) or UserTeam.find(params[:awayteam_id])
-    @match.hometeam = home
-    @match.awayteam = away
-    @match.simulate
-    @match.save
+    if params[:home_user_team_id] == ""
+      home = Team.find(params[:home_team_id])
+    else
+      home = UserTeam.find(params[:home_user_team_id])
+    end
+
+    if params[:away_user_team_id] == ""
+      away = Team.find(params[:away_team_id])
+    else
+      away = UserTeam.find(params[:away_user_team_id])
+    end
+   # home = Team.find(params[:home_team_id]) or UserTeam.find(params[:home_user_team_id])
+   # away = Team.find(params[:away_team_id]) or UserTeam.find(params[:away_user_team_id])
+   @match.hometeam = home
+   @match.awayteam = away
+
+   @match.simulate
+   @match.save
   end
 end
