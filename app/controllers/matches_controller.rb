13f6c10e0ce4cdf8085983b8696e91c576bf0bec
SchruteFarms23 <romanmullady@gmail.com>
2017-08-22 15:37:22 -0400

    matches new

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,12 @@
 class MatchesController < ApplicationController
+
+  def new
+    @match = Match.new
+    @teams = Team.all
+    @user_teams = UserTeam.all
+  end
+
+  def create
+    @match = Match.new
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ApplicationRecord
   belongs_to :team, optional: true
   has_many :player_user_teams
   has_many :user_teams, through: :player_user_teams
-  delegate :league, to: :team
+  delegate :league, to: :team, allow_nil: true
 
   validates :name, presence: true
   validates :shooting,:passing,:defense,:foul,:dribbling, presence: true
